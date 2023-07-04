f9a8335c2df9c6a9693bdde5733e952500f4afbe
anhtuan5121996@gmail.com <anhtuan5121996@gmail.com>
2023-07-05 00:08:29 +0700

    first commit

---
+++ AspNetIdentity/Repositories/Abstract/IUserAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using AspNetIdentity.Models.DTO;
+
+namespace AspNetIdentity.Repositories.Abstract
+{
+    public interface IUserAuthenticationService
+    {
+        Task<Status> LoginAsync(LoginModel model);
+        Task<Status> RegistrationAsync(RegistrationModel model);   
+        Task<Status> ChangePasswordAsync(ChangePasswordModel model, string username);   
+        Task LogoutAsync();
+    }
+}
